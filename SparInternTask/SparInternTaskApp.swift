71cb6639e9259e6f83a766d3eb644eaf9956574e
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-08-08 17:43:56 +0300

    Initial Commit

---
+++ SparInternTask/SparInternTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparInternTaskApp.swift
+//  SparInternTask
+//
+//  Created by Александр Плешаков on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparInternTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

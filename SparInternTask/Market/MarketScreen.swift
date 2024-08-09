e1a05220c2b2db2518f2f091cb3c34f58fb6f968
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-08-09 14:38:54 +0300

    Add market view model and changing layout  state

--- SparInternTask/Market/MarketScreen.swift
+++ SparInternTask/Market/MarketScreen.swift
@@ -9,25 +9,31 @@ import SwiftUI
 
 struct MarketScreen: View {
     private let columns = [
-            GridItem(.flexible(), spacing: 16),
+            GridItem(.flexible(), spacing: 5),
             GridItem(.flexible())
     ]
     
-    @State var isGridLayout = true
     
-    init(isGridLayout: Bool = true) {
-        self.isGridLayout = isGridLayout
+    @ObservedObject var viewModel: MarketViewModel
+    
+    init(viewModel: MarketViewModel, isGridLayout: Bool = true) {
+        self.viewModel = viewModel
         NavigationBarAppearance.setCustomAppearance()
     }
     
     var body: some View {
         NavigationView {
-            LazyVGrid(columns: columns) {
-                
+            Group {
+                switch viewModel.layoutState {
+                case .grid:
+                    LazyVGrid(columns: columns, spacing: 8) {}
+                case .list:
+                    List {}
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .principal) {
-                    LayoutButton(isGridLayout: $isGridLayout)
+                    LayoutButton(onTap: viewModel.changeState, layout: viewModel.layoutState)
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
@@ -36,15 +42,25 @@ struct MarketScreen: View {
 }
 
 fileprivate struct LayoutButton: View {
-    @Binding var isGridLayout: Bool
+    let onTap: () -> Void
+    let layout: MarketLayoutState
+    @State var isGridLayout: Bool
+    
+    init(onTap: @escaping () -> Void, layout: MarketLayoutState) {
+        self.onTap = onTap
+        self.layout = layout
+        self.isGridLayout = layout == .grid
+    }
+    
     var body: some View {
         HStack {
             Spacer()
                 .frame(width: isGridLayout ? 0 : 8)
             Button {
-                withAnimation {
+                withAnimation(.easeIn(duration: 0.1)) {
                     isGridLayout.toggle()
                 }
+                onTap()
             } label: {
                 Image(
                     uiImage: isGridLayout ? Images.gridLayout : Images.listLayout
@@ -56,5 +72,5 @@ fileprivate struct LayoutButton: View {
 }
 
 #Preview {
-    MarketScreen()
+    MarketScreen(viewModel: MarketViewModel())
 }
---
+++ SparInternTask/Market/ViewModels/MarketViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  MarketViewModel.swift
+//  SparInternTask
+//
+//  Created by Александр Плешаков on 09.08.2024.
+//
+
+import Foundation
+
+enum MarketLayoutState {
+    case grid, list
+}
+
+final class MarketViewModel: ObservableObject {
+    @Published var layoutState: MarketLayoutState = .grid
+    
+    func changeState() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+            guard let self else { return }
+            layoutState = layoutState == .grid ? .list : .grid
+        }
+        
+    }
+}

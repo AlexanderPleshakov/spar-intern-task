92a3dac112bcbc645ebb0b4fb9f7b6049bdf50c4
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-08-09 13:29:28 +0300

    Add resource images file

---
+++ SparInternTask/Resources/Images.swift
@@ -0,0 +1,13 @@
+//
+//  Images.swift
+//  SparInternTask
+//
+//  Created by Александр Плешаков on 09.08.2024.
+//
+
+import SwiftUI
+
+enum Images {
+    static let gridLayout = UIImage(named: "GridLayout") ?? UIImage()
+    static let listLayout = UIImage(named: "ListLayout") ?? UIImage()
+}

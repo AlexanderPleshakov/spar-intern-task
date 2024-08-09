413e197a43ac6f56b0d572810680e202b5772549
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-08-09 16:44:18 +0300

    Add text colors

--- SparInternTask/Resources/Colors.swift
+++ SparInternTask/Resources/Colors.swift
@@ -11,4 +11,10 @@ enum Colors {
     static let badgeRed = Color(hex: "#FC6A6F").opacity(0.9)
     static let badgeGreen = Color(hex: "#5BCD7B").opacity(0.9)
     static let badgeBlue = Color(hex: "#7A79BA").opacity(0.9)
+    
+    static let textStatic = Color(.black).opacity(0.8)
+    
+    static let textIconsBlack01 = Color(hex: "#262626")
+    static let textIconsBlack02 = Self.textIconsBlack01.opacity(0.8)
+    static let textIconsBlack03 = Self.textIconsBlack01.opacity(0.6)
 }
